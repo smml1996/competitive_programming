bc595998544412e267b440e7e8aec561d33042c4
smml1996 <michelle_muroya@hotmail.com>
2018-01-23 22:45:03 -0500

    Update strassen.cpp

--- strassen.cpp
+++ strassen.cpp
@@ -27,6 +27,18 @@ void rellenarMatrix(vector< vector<int> > A, vector< vector<int> > &a11,vector< 
     }
 }
 
+void imprimir(vector< vector<int> > t){
+
+    for(int i = 0; i< t.size(); i++){
+
+        for(int j = 0; j< t.size(); j++){
+            cout << t[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+}
 
 vector< vector<int> > sumar(vector< vector<int> > A, vector< vector<int> > B){
     vector< vector<int> > answer(A.size(), vector<int>(A.size()));
@@ -76,35 +88,40 @@ vector< vector<int> > multiplicar(vector< vector<int> > A, vector< vector<int> >
         }
 
     }
+
     return answer;
 }
 
 
+
+
 vector< vector<int> > strassen(vector< vector<int> > a, vector< vector<int> > b, int n){
 
-    if(a.size() <= 2){
-        return multiplicar(a,b);
+    if(a.size() <=2){
+        vector< vector<int> >t =  multiplicar(a,b);
+        return t;
     }
 
     vector< vector<int> > a11( n/2, vector<int>(n/2) ) , a22(n/2, vector<int>(n/2)), a21(n/2, vector<int>(n/2)),a12(n/2, vector<int>(n/2));
     vector< vector<int> > b11( n/2, vector<int>(n/2) ), b22(n/2, vector<int>(n/2)), b21(n/2, vector<int>(n/2)) , b12(n/2, vector<int>(n/2));
 
     rellenarMatrix(a, a11, a22, a21, a12,n);
+
     rellenarMatrix(b, b11, b22, b21, b12,n);
 
+
     vector< vector<int> > p = strassen(sumar(a11, a22), sumar(b11, b22), n/2);
+    vector< vector<int> > q = strassen(sumar(a21, a22),b11, n/2);
+    vector< vector<int> > r = strassen(a11, restar(b12, b22), n/2);
+
+    vector< vector<int> > s = strassen(a22, restar(b21, b11), n/2);
+
+    vector< vector<int> > t = strassen(sumar(a11, a12), b22, n/2);
+    vector< vector<int> > u = strassen(restar(a21, a11), sumar(b11,b12),n/2);
+    vector< vector<int> > v = strassen(restar(a12, a22), sumar(b21,b22), n/2);
 
 
-    vector< vector<int> > q = strassen(sumar(a12, a22),b11, b11.size());
-    vector< vector<int> > r = strassen(a11, restar(b12, b22), a11.size());
-    vector< vector<int> > s = strassen(a22, restar(b21, b11), a22.size());
-
-    vector< vector<int> > t = strassen(sumar(a11, a12), b22, b22.size());
-    vector< vector<int> > u = strassen(restar(a21, a11), sumar(b12,b11), a11.size());
-    vector< vector<int> > v = strassen(restar(a12, a22), sumar(b21,b22), a11.size());
-
-
-    vector< vector<int> > c11 = sumar(restar(sumar(p,s), t), u);
+    vector< vector<int> > c11 = sumar(restar(sumar(p,s), t), v);
     vector< vector<int> > c12 = sumar(r,t);
     vector< vector<int> > c21 = sumar(q,s);
     vector< vector<int> > c22 = sumar(restar(sumar(p,r), q), u);
@@ -115,16 +132,16 @@ vector< vector<int> > strassen(vector< vector<int> > a, vector< vector<int> > b,
     for(int i = 0; i < n ; i++){
 
         for(int j = 0; j < n; j++){
-            if(i < n/2){
+            if(i < (n/2)){
 
-                if(j < n/2)
+                if(j < (n/2))
                     temp = c11[i][j];
                 else
-                    temp = c12[i][j - n/2];
-            }else if( j < n/2){
-                temp = c21[i - n/2][j];
+                    temp = c12[i][j - (n/2)];
+            }else if( j < (n/2)){
+                temp = c21[i - (n/2)][j];
             }else{
-                temp = c22[i - n/2][j - n/2];
+                temp = c22[i -(n/2)][j - (n/2)];
             }
 
             answer[i][j] = temp;
@@ -168,7 +185,16 @@ int main(){
         }
     }
 
-    strassen(a,b,n);
+    vector< vector<int> > s = strassen(a,b,n);
 
-return 0;
+    cout << endl << "RESPUESTA: " << endl;
+    for(int i =0; i< s.size(); i++){
+
+        for(int j =0; j< s.size(); j++){
+            cout << s[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
