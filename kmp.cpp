394e0a1c2c9cc6d93271b55575825ca91002c8ab
smml1996 <michelle_muroya@hotmail.com>
2018-02-08 06:34:45 -0500

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -1,56 +1,52 @@
 #include <iostream>
+#include<string>
 #include <vector>
-#include <string>
+#include <fstream>
+#include <ctime>
 
 using namespace std;
-template <class T>
-class KMP
-{
-    vector<int> preprocess(const vector<T> &pattern)
-    {
-        vector<int> result( pattern.size(), 0 );
 
-        for( int i = 1; i < pattern.size(); i++ )
-        {
-            if(pattern.at(result[i-1]) == pattern.at(i))
-            {
-                result[i] = result[i-1] + 1;
-            }
+
+string pattern;
+
+vector<int> preprocess(const string &pattern) {
+    vector<int> result( pattern.size(), 0 );
+
+    for( int i = 1; i < pattern.size(); i++ ) {
+
+        if(pattern.at(result[i-1]) == pattern.at(i)){
+            result[i] = result[i-1] + 1;
         }
-
-        return result;
     }
-public:
 
-    int operator()(const vector<T> &text, const vector<T> &pattern)
+    return result;
+}
+
+int kmp(const string &text, const string &pattern)
+{
+    vector<int> preprocessTable = preprocess(pattern);
+    int match_length;
+    for(int i = 0; i < text.size() - pattern.size() + 1 ;)
     {
-        vector<int> preprocessTable = preprocess(pattern);
-        int match_length;
-        for(int i = 0; i < text.size() - pattern.size() + 1 ;)
-        {
-            match_length = 0;
+        match_length = 0;
 
-            if(text.at(i) == pattern.at(0))
-            {
+        if(text.at(i) == pattern.at(0)) {
+            match_length++;
+
+            while( match_length < pattern.size() && pattern.at( match_length ) == text.at( i + match_length ) )
                 match_length++;
 
-                while( match_length < pattern.size() && pattern.at( match_length ) == text.at( i + match_length ) )
-                    match_length++;
-
-                if(match_length == pattern.size())
-                    return i;
-                i += match_length - preprocessTable[match_length -1];
-            }else
-                i++;
-        }
-
-        return -1;
+            if(match_length == pattern.size())
+                return i;
+            i += match_length - preprocessTable[match_length -1];
+        }else
+            i++;
     }
 
-};
+    return -1;
+}
 
-template <class T>
-int naiveStringMatching(vector<T> text, vector<T> pattern)
+int naiveStringMatching(string text, string pattern)
 {
     for(int i = 0; i < text.size() - pattern.size() + 1; i++)
     {
@@ -61,37 +57,56 @@ int naiveStringMatching(vector<T> text, vector<T> pattern)
     return -1;
 }
 
-int main()
-{
-    /*
-    vector<int> r = preprocess("abababca");
+int main(){
+
+    ofstream kmpFile;
+    kmpFile.open("kmp.txt");
+
+    ofstream naiveFile;
+    naiveFile.open("naive.txt");
+    clock_t t;
+
+    for(int i =0; i< 1000; i++){
+
+        pattern+='a';
+
+    }
 
 
-    for( int i = 0; i < r.size(); i++ )
-        cout << r[i] <<  " ";
 
-    cout << endl;
-    */
+    for(int i =10000; i <= 1000000; i+=10000){
 
-    vector<char> text , pattern;
+        string text = "";
+        for(int j =0; j < i -pattern.size(); j++){
 
-    text.push_back('c');
-    text.push_back('b');
-    text.push_back('a');
-    text.push_back('b');
-    text.push_back('c');
-    text.push_back('a');
-    text.push_back('b');
-    text.push_back('a');
+            if( (text.size() + 1) % pattern.size()== 0){
+                text+='b';
+            }else{
+                text+='a';
+            }
+        }
 
-    pattern.push_back('a');
-    pattern.push_back('b');
-    pattern.push_back('a');
-    KMP<char> kmp;
-    cout << kmp(text, pattern) << endl;
+        text+=pattern;
 
-    cout << naiveStringMatching(text, pattern) << endl;
+        t = clock();
+        kmp(text, pattern);
+        t = clock() - t;
+        kmpFile << ( ( ( float ) t ) / CLOCKS_PER_SEC );
+        kmpFile << endl;
+
+        t = clock();
+        naiveStringMatching(text, pattern);
+        t = clock() - t;
+        naiveFile << ( ( ( float ) t ) / CLOCKS_PER_SEC );
+        naiveFile << endl;
+
+
+    }
+
+    kmpFile.close();
+    naiveFile.close();
 
 
     return 0;
 }
+
